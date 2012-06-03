20f2673438842762dd7d6fb82ef416cc2dfc26d4
DLMSweet <tyler@tsweet.net>
2012-06-03 17:50:19 -0500

    Removed older files not needed

--- bin/genKippoPlaylogs.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
- for FILE in $(ls -l /opt/kippo/log/tty/*.log | awk '$5>2128' | cut -d '/' -f6); do echo \<a href="playlog/?l=$FILE"\>$FILE\</a\>\<br /\>; done
--- bin/kippo_loginAttempts.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "<table border=1>"
-echo "<tr><td>Times Used</td><td>Username</td><td>Password</td></tr>"
-mysql kippo -u$1 -p$2 --skip-column-names -Be "SELECT username,password FROM auth;" | sort -k1 | uniq -c | sort -nrk1 | awk '{print "<tr><td>"$1"</td><td>"$2"</td><td>"$3"</td></tr>"}'
-echo "</table>"
