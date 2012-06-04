2b74377ccf2834959a615d78b6db9883225f810c
DLMSweet <tyler@tsweet.net>
2012-06-03 19:19:56 -0500

    Modified main query to remove errors and such.

--- index.php
+++ index.php
@@ -5,11 +5,9 @@ include "header.php";
 
 mysql_connect($server,$username,$password);
 @mysql_select_db($database) or die( "Unable to select database");
-//$query = "select sessions.id,sensor,ip,starttime,endtime FROM sessions LEFT JOIN auth ON sessions.id=auth.session WHERE ( SELECT TIMEDIFF(endtime, starttime)>60 AND success=1)";
-$query = "select sessions.id,sensor,ip,starttime,endtime FROM sessions LEFT JOIN auth ON sessions.id=auth.session WHERE success=1";
+$query = "SELECT sessions.id,sensors.ip AS sensor_ip,sessions.ip AS session_IP,starttime,endtime FROM sessions LEFT JOIN auth ON sessions.id=auth.session LEFT JOIN sensors ON sensors.id=sessions.sensor WHERE success=1;";
 $result=mysql_query($query);
 $num=mysql_numrows($result);
-
 mysql_close();
 echo "<table border=1>\n";
 echo "<tr><td><b>Session ID</b></td><td><b>Sensor</b></td><td><b>IP</b></td><td><b>Start Time</b></td><td><b>End Time</b></td><td><b>Commands Run</b></td></tr>\n";
@@ -17,8 +15,8 @@ echo "<tr><td><b>Session ID</b></td><td><b>Sensor</b></td><td><b>IP</b></td><td>
 $i=0;
 while ($i < $num) {
     $id=mysql_result($result,$i,"id");
-    $sensor_id=mysql_result($result,$i,"sensor");
-    $ip=mysql_result($result,$i,"ip");
+    $sensor=mysql_result($result,$i,"sensor_ip");
+    $ip=mysql_result($result,$i,"session_IP");
     $starttime=mysql_result($result,$i,"starttime");
     $endtime=mysql_result($result,$i,"endtime");
 
@@ -26,10 +24,6 @@ while ($i < $num) {
     @mysql_select_db($database) or die( "Unable to select database");
     $input_count_query = "select input from input where session='$id'";
     $input_count=mysql_numrows(mysql_query($input_count_query));
-
-    $query_getSensor = "select ip from sensors where id='$sensor_id'";
-    $result_getSensor = mysql_query($query_getSensor);
-    $sensor = mysql_result($result_getSensor,0,"ip");
 
     mysql_close();
     if ( $input_count > 0 ) {
