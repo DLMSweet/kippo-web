ca4defffab61715fdc7cb71783d4d3d78a8ab10f
thefinn93 <thefinn93@thefinn93.com>
2013-06-16 19:57:36 -0700

    fixed some shit

--- assets/js/kippoweb.js
+++ assets/js/kippoweb.js
@@ -2,6 +2,5 @@ $(document).ready(function() {
     window.showSpoiler = function(obj) {
         $(obj.parentNode.getElementsByTagName('div')[0]).collapse('toggle')
     }
-    $(".inner").each(function(i) {$($(".inner")[i]).collapse('hide');})
-    }
+}
 )
